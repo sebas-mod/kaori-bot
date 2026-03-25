07a304de932335a52b5c4edc9712af9619550540
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-03-25 18:11:32 -0300

    Update menu.js

--- commands/main/menu.js
+++ commands/main/menu.js
@@ -99,7 +99,7 @@ export default {
               },
               externalAdReply: {
                 title: botname,
-                body: `${namebot}, mᥲძᥱ ᥕі𝗍һ ᑲᥡ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜`,
+                body: `${namebot}, p̶o̶w̶e̶r̶e̶d̶ ̶b̶y̶ ̶:̶ ̶s̶e̶b̶a̶s̶ ̶m̶d̶`,
                 showAdAttribution: false,
                 thumbnailUrl: banner,
                 mediaType: 1,
